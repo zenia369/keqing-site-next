de6a73c2ab2f2b1f63894d99201436ce31a58eae
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-14 12:44:18 +0300

    refactor: add metadata on characters page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import Characters from "@/pages/characters/Characters";
+
+export const metadata: Metadata = {
+  title: "Keqing | Characters",
+  description:
+    "Привіт, і це Keqing-site. Семпай може тут знайти багато цікавого контенту(фото, відео) по світу ігор, манг та щіпотку цікавого функціоналу.",
+};
 
 export default function Page() {
   return <Characters />;
