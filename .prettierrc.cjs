316b43b24ff83b246b3b7770c79e36df4268b6ff
Kushnir E <kyshnirxeneea@gmail.com>
2023-10-21 15:16:42 +0300

    refactor: change print windth in prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   tabWidth: 2,
   semi: true,
   singleQuote: false,
-  jsxSingleQuote: false
-}
+  jsxSingleQuote: false,
+  printWidth: 100,
+};
