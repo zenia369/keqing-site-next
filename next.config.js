47a5f5811baa4f62566ccc31d676c3a45a4a681d
Kushnir E. <kyshnirxeneea@gmail.com>
2024-09-14 23:14:12 +0300

    chore: update git ignore, tsconfig and next config files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/",
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -29,4 +21,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
