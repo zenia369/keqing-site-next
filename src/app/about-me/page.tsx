8ae36751f84730d8f0764951e245154b8205c3b8
Kushnir E. <kyshnirxeneea@gmail.com>
2024-09-14 23:41:41 +0300

    refactor: update about me page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,10 +1,11 @@
 import { Metadata } from "next";
 
+import aboutBG from "@/assets/images/about-bg.png";
 import Footer from "@/components/ui/footer/Footer";
 import Main from "@/components/ui/main/Main";
-import Page from "@/components/ui/page/Page";
 import Navigation from "@/components/ui/navigation/Navigation";
-import Overlay from "@/components/pages/about-me/Overlay";
+import Overlay from "@/components/ui/overlay/Overlay";
+import Page from "@/components/ui/page/Page";
 
 export const metadata: Metadata = {
   title: "Keqing | AboutMe",
@@ -14,22 +15,20 @@ export const metadata: Metadata = {
 
 export default function Route() {
   return (
-    <Overlay>
+    <Overlay backgroundUrl={aboutBG.src}>
       <Page classes="h-[100vh]">
         <Navigation pageName="About" isShowSendMessageLink />
         <Main>
           <div className="flex justify-center items-center gap-10 mt-[20%]">
             <div>
-              <h2 className="underline text-4xl text-white font-bold">
-                Kushnir Yevhenii
-              </h2>
+              <h2 className="underline text-4xl text-white font-bold">Kushnir Yevhenii</h2>
               <ul className="list-[circle] mt-5 ml-5 text-lg">
                 <li className="text-white">Front-end Dev.</li>
               </ul>
             </div>
             <p className="text-white w-96 text-lg">
-              Цей сайт є проектом автора. Також з часом буде покращуватись
-              візуально та функціонально. © Kushnir
+              Цей сайт є проектом автора. Також з часом буде покращуватись візуально та
+              функціонально. © Kushnir
             </p>
           </div>
         </Main>
