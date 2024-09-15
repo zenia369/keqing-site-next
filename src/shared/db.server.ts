1b40d9cbc0a4762a47a141217b4354da2678f661
Kushnir E. <kyshnirxeneea@gmail.com>
2024-09-15 13:16:58 +0300

    fix: resolve issue with prisma multiple instances

--- src/shared/db.server.ts
+++ src/shared/db.server.ts
@@ -1,36 +1,34 @@
 import { PrismaClient } from "@prisma/client";
 import invariant from "tiny-invariant";
 
-const prismaClientSingleton = () => {
-  const { DATABASE_URL } = process.env;
-  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
+const { DATABASE_URL } = process.env;
+invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
 
-  const databaseUrl = new URL(DATABASE_URL);
+const databaseUrl = new URL(DATABASE_URL);
 
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
-  // NOTE: during development if you change anything in this function, remember
-  // that this only runs once per server restart and won't automatically be
-  // re-run per request like everything else is. So if you need to change
-  // something in this file, you'll need to manually restart the server.
-  const client = new PrismaClient({
+console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
     datasources: {
       db: {
         url: databaseUrl.toString(),
       },
     },
   });
-  // connect eagerly
-  client.$connect();
-
-  return client;
-};
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: databaseUrl.toString(),
+        },
+      },
+    });
+  }
+  prisma = (global as any).prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
